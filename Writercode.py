aca97419ea6fca2d1f73a8002c3ebfd4a67020b6
superjakeyLKR <82766388+superjakeyLKR@users.noreply.github.com>
2021-04-29 15:18:22 +0100

    Update Writercode.py
    Testing a 1.1 prototype.

--- Writercode.py
+++ Writercode.py
@@ -1,7 +1,10 @@
 import pyautogui, time
 pyautogui.FAILSAFE = true
+textSpeed = float(input("Enter write speed in seconds"))
 time.sleep(5)
 text = open("texttowrite", 'r')
 for word in text:
-pyautogui.typewrite(word)
-pyautogui.press("enter")
+    pyautogui.typewrite(word)
+    pyautogui.press("enter")
+    if textSpeed > 0:
+       time.sleep(textSpeed)
