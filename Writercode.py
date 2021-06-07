12aba1a9abd0a32ee40720e5e7d2ba7fc0b40f0c
superjakeyLKR <82766388+superjakeyLKR@users.noreply.github.com>
2021-06-07 15:53:55 +0100

    Update Writercode.py
    forgot the ".txt"

--- Writercode.py
+++ Writercode.py
@@ -1,7 +1,7 @@
 import pyautogui, time
 pyautogui.FAILSAFE = True
 time.sleep(5)
-text = open("texttowrite", 'r')
+text = open("texttowrite.txt", 'r')
 for word in text:
   pyautogui.typewrite(word)
   pyautogui.press("enter")
