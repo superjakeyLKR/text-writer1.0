e9020aa5d0450b9b83185f3a07c999d8566b47ed
superjakeyLKR <82766388+superjakeyLKR@users.noreply.github.com>
2021-07-01 11:38:44 +0100

    Update Writercode.py
    updated it again

--- Writercode.py
+++ Writercode.py
@@ -2,13 +2,13 @@ import pyautogui, time
 pyautogui.FAILSAFE = True
 oneWordSpam = False
 text = open("texttowrite.txt", 'r')
-a = int(input("Spam just one word? (0 for no, 1 for yes) "))
-if a == 0:
+a = input("Spam just one word? (0 for no, 1 for yes) ")
+if a == "0":
   time.sleep(5)
   for word in text:
     pyautogui.typewrite(word)
     pyautogui.press("enter")
-else:
+elif a == "1":
   b = int(input("How many times would you like to spam it? "))
   if b <= 0:
     print("Sorry, but that is an invalid number")
@@ -20,4 +20,7 @@ else:
       pyautogui.typewrite(t)
       pyautogui.press("enter")
       i += 1
+else:
+  print(a +" is an invalid input.")
+  quit()
 print("Done writing!")
